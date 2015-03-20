3d04015f0328632a68a48b1bd67031e0e3fd104f
Michael Shirk <wmichaelshirk@gmail.com>
2015-03-20 14:07:16 -0400

    added compass process

--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,39 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
     concat = require('gulp-concat'),
-    browserify = require('gulp-browserify');
+    browserify = require('gulp-browserify'),
+    compass = require('gulp-compass');
 
-var coffeeSources = ['components/coffee/*.coffee']
+var coffeeSources = ['components/coffee/*.coffee'];
 var jsSources = [
-	'components/scripts/rclick.js',
-	'components/scripts/pixgrid.js',
-	'components/scripts/tagline.js',
-	'components/scripts/template.js'
-]
+    'components/scripts/rclick.js',
+    'components/scripts/pixgrid.js',
+    'components/scripts/tagline.js',
+    'components/scripts/template.js'
+];
+var sassSources = ['components/sass/style.scss'];
 
-gulp.task('coffee', function(){
-	gulp.src(coffeeSources)
-	.pipe(coffee({ bare: true })
-		.on('error', gutil.log))
-	.pipe(gulp.dest('components/scripts'))
+gulp.task('coffee', function () {
+    gulp.src(coffeeSources)
+    .pipe(coffee({ bare: true })
+        .on('error', gutil.log))
+    .pipe(gulp.dest('components/scripts'));
 });
 
-gulp.task('js', function() {
-	gulp.src(jsSources)
-	.pipe(concat('script.js'))
-	.pipe(browserify())
-	.pipe(gulp.dest('builds/development/js'));
-})
+gulp.task('js', function () {
+    gulp.src(jsSources)
+    .pipe(concat('script.js'))
+    .pipe(browserify())
+    .pipe(gulp.dest('builds/development/js'));
+});
+
+gulp.task('compass', function () {
+    gulp.src(sassSources)
+    .pipe(compass({
+        sass: 'components/sass',
+        image: 'builds/development/images',
+        style: 'expanded'
+    })
+	.on('error', gutil.log))
+	.pipe(gulp.dest('builds/development/css'));
+});

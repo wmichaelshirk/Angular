9e199af0f731126dabb7d95a62f46f1e5bd84b92
Michael Shirk <wmichaelshirk@gmail.com>
2015-03-25 15:28:58 -0400

    Minified HTML files

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var gulp = require('gulp'),
     connect = require('gulp-connect'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
+    minifyHTML = require('gulp-minify-html'),
     compass = require('gulp-compass');
 
 var env,
@@ -67,7 +68,9 @@ gulp.task('compass', function () {
 });
 
 gulp.task('html', function() {
-    gulp.src(htmlSources)
+    gulp.src('builds/development/*.html')
+      .pipe(gulpif(env === 'production', minifyHTML()))
+      .pipe(gulpif(env === 'production', gulp.dest(outputDir)))
       .pipe(connect.reload());
 });
 
@@ -80,7 +83,7 @@ gulp.task('watch', function() {
     gulp.watch(coffeeSources, ['coffee']);
     gulp.watch(jsSources, ['js']);
     gulp.watch('components/sass/*.scss', ['compass']);
-    gulp.watch(htmlSources, ['html']);
+    gulp.watch('builds/development/*.html', ['html']);
     gulp.watch(jsonSources, ['json']);
 });
 
